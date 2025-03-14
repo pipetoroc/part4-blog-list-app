fa4ac4ae68a1f3da86eb85c23ead6f428c91eaeb
Felipe Toro <ftorocastilla@gmail.com>
2025-03-14 16:55:11 -0300

    feat: resolve exercise 4.4 testing

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((totalLikes, blog) => totalLikes + blog.likes, 0)
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
