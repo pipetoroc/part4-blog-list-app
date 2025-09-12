b5e9f2045d52f398fd89902b9017589fe2eb79ab
pipetoroc <ftorocastilla@gmail.com>
2025-09-12 19:59:29 +0100

    resolve exercise 4.17 adding populate in blogs and users

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,8 +1,12 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1
+  })
   response.json(blogs)
 })
 
@@ -20,6 +24,8 @@ blogRouter.get('/:id', (request, response, next) => {
 blogRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'content missing' })
   }
@@ -28,11 +34,15 @@ blogRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
-  await response.status(201).json(savedBlog)
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogRouter.delete('/:id', (request, response, next) => {
