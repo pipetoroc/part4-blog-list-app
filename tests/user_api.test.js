bab24f7f2d16513a60b0123043f4ff604417466f
pipetoroc <ftorocastilla@gmail.com>
2025-09-17 17:46:11 +0100

    resolve exercise 4.23 refactor and improve tests

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -3,34 +3,87 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('node:assert')
 const bcrypt = require('bcrypt')
-const User = require('../models/user')
-const helper = require('./tests_helper')
 
 const app = require('../app')
 const api = supertest(app)
 
-// beforeEach(async () => {
-//   await User.deleteMany({})
-//   let userObject = new User(helper.initialUsers[0])
-//   await userObject.save()
-//   userObject = new User(helper.initialUsers[1])
-//   await userObject.save()
-// })
+const User = require('../models/user')
+const Blog = require('../models/blog')
 
-describe('creating a new user', () => {
+let token = ''
+
+const createUserAndLogin = async ({ username, password }) => {
+  const passwordHash = await bcrypt.hash(password, 10)
+  const user = new User({ username, passwordHash })
+  await user.save()
+
+  const loginResponse = await api
+    .post('/api/login')
+    .send({ username, password })
+    .expect(200)
+
+  return loginResponse.body.token
+}
+
+const getAllBlogs = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(b => b.toJSON())
+}
+
+const getAllUsers = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
+describe('Blog API tests', () => {
   beforeEach(async () => {
+    await Blog.deleteMany({})
     await User.deleteMany({})
 
-    const passwordHash = await bcrypt.hash('pswd', 10)
-    const user = new User({ username: 'miduroot', passwordHash })
-
-    await user.save()
+    token = await createUserAndLogin({
+      username: 'miduroot',
+      password: 'pswd'
+    })
   })
 
-  test('works as expected creating a fresh username', async () => {
-    const usersDB = await User.find({})
-    console.log(usersDB)
-    const usersAtStart = usersDB.map(user => user.toJSON())
+  test('a valid blog can be added with a token', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: 'Someone',
+      url: 'http://example.com',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await getAllBlogs()
+    assert.strictEqual(blogsAtEnd.length, 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('async/await simplifies making async calls'))
+  })
+
+  test('adding a blog fails with 401 if token is not provided', async () => {
+    const newBlog = {
+      title: 'This should not be saved',
+      author: 'Hacker',
+      url: 'http://evil.com',
+      likes: 0
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(401)
+
+    const blogsAtEnd = await getAllBlogs()
+    assert.strictEqual(blogsAtEnd.length, 0)
+  })
+
+  test('a fresh username can be created', async () => {
+    const usersAtStart = await getAllUsers()
 
     const newUser = {
       username: 'midudev',
@@ -44,54 +97,13 @@ describe('creating a new user', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const usersDBAfter = await User.find({})
-    console.log(usersDBAfter)
-    const usersAtEnd = usersDBAfter.map(user => user.toJSON())
-
+    const usersAtEnd = await getAllUsers()
     assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes('midudev'))
   })
 })
-// describe('get requests', () => {
-//   test('users are returned as json', async () => {
-//     await api
-//       .get('/api/users')
-//       .expect(200)
-//       .expect('Content-Type', /application\/json/)
-//   })
-//   test('there are two users in the DB', async () => {
-//     const response = await api.get('/api/users')
-//     assert.strictEqual(response.body.length, helper.initialUsers.length)
-//   })
-// })
-
-// describe('post requests', () => {
-//   test('Username and password must be given', async () => {
-//     const newUser = {
-//       username: 'fetora',
-//       password: 'la pipe password',
-//       name: 'Felipe'
-//     }
-
-//     await api
-//       .post('/api/users')
-//       .send(newUser)
-//       .expect(201)
-//       .expect('Content-Type', /application\/json/)
-//   })
-//   test('The password and username must be at least 3 characters long', async () => {
-//     const newUser = {
-//       username: 'fetora2',
-//       password: 'la pipe password',
-//       name: 'Felipe'
-//     }
-
-//     await api
-//       .post('/api/users')
-//       .send(newUser)
-//       .expect(201)
-//       .expect('Content-Type', /application\/json/)
-//   })
-// })
 
 after(async () => {
   await mongoose.connection.close()
